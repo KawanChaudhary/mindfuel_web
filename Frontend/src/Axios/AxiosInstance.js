508b5899a6869dd4032fb0f4b176115281e56421
Kawan Chaudhary <kawachaudhary@gmail.com>
2024-05-23 13:32:59 +0530

    bugs fixed

--- Frontend/src/Axios/AxiosInstance.js
+++ Frontend/src/Axios/AxiosInstance.js
@@ -1,8 +1,8 @@
 const axios = require('axios');
 
 const instance = axios.create({
-  // baseURL: `${process.env.REACT_APP_BACKEND_URI}`,
-  baseURL: 'http://localhost:5000',
+  baseURL: `${process.env.REACT_APP_BACKEND_URI}`,
+  // baseURL: 'http://localhost:5000',
   headers: {
     'Content-Type': 'application/json', 
   },
